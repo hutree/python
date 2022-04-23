6d15ea69af552b78aa8a5465c3b21fba3fbbf50a
hutree <1120654932@qq.com>
2022-04-23 12:38:32 +0800

    初始化

---
+++ htmlFindPlace.py
@@ -0,0 +1,10 @@
+from time import sleep
+
+from selenium import webdriver
+
+webDriver = webdriver.Chrome()
+webDriver.get('https://www.baidu.com/')
+inputKw = webDriver.find_element_by_id('kw')
+inputKw.send_keys("民族")
+sleep(2)
+webDriver.find_element_by_xpath('//*[@id="su"]').click()
